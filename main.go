2664a185cf911fdf0921967ec65432d10c13bf77
sweetcatjiang <122420884+sweetcatjiang@users.noreply.github.com>
2023-01-13 11:24:28 +0800

    Update main.go

--- main.go
+++ main.go
@@ -205,7 +205,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", ".", "Set default nickname.")
+	阿达纳 ：= 标志。字符串（“a”， “.”， “设置默认昵称”。)
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
